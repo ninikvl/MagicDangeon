e32e1879a08946c8231ea46cd6fc3d5188593d8d
ninikvl <93017938+ninikvl@users.noreply.github.com>
2023-05-12 17:11:04 +1000

    Малые исправление RoomTemplateSO

--- MagicDungeon/Assets/Scripts/Dungeon/RoomTemplateSO.cs
+++ MagicDungeon/Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -27,27 +27,27 @@ public class RoomTemplateSO : ScriptableObject
     #endregion ROOM CONFIGURATION
 
     #region Tooltip
-    [Header("Тип узла комнаты. (Типом корридоа не может быть 'Corridor') ")]
+    [Tooltip("Тип узла комнаты. (Типом корридоа не может быть 'Corridor') ")]
     #endregion Tooltip
     public RoomNodeTypeSO roomNodeType;
 
     #region Tooltip
-    [Header("Координаты нижнего левого угла в Tilemap (не World Coordinates)")]
+    [Tooltip("Координаты нижнего левого угла в Tilemap (не World Coordinates)")]
     #endregion Tooltip
     public Vector2Int lowerBounds;
 
     #region Tooltip
-    [Header("Координаты верхнего правого угла в Tilemap (не World Coordinates)")]
+    [Tooltip("Координаты верхнего правого угла в Tilemap (не World Coordinates)")]
     #endregion Tooltip
     public Vector2Int upperBounds;
 
     #region Tooltip
-    [Header("В комнате максимум 4 дверных прооёма, по одному на каждое направление. Положение средней плитки является координатоой дверного проёма (position).")]
+    [Tooltip("В комнате максимум 4 дверных прооёма, по одному на каждое направление. Положение средней плитки является координатоой дверного проёма (position).")]
     #endregion Tooltip
     [SerializeField] public List<Doorway> doorwaysList;
 
     #region Tooltip
-    [Header("Массив позиций в которых могут спавниться сундуки и враги (кооординаты берутся из Tilemap)")]
+    [Tooltip("Массив позиций в которых могут спавниться сундуки и враги (кооординаты берутся из Tilemap)")]
     #endregion Tooltip
     public Vector2Int[] spawnPosArray;
 
