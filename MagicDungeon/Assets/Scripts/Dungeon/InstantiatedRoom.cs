bfbeb426178c7e495614836c047debda2c96fdbe
ninikvl <93017938+ninikvl@users.noreply.github.com>
2023-05-15 16:43:50 +1000

    Готовый строитель подземелий + блокировка неиспользуемых проёмов

--- MagicDungeon/Assets/Scripts/Dungeon/InstantiatedRoom.cs
+++ MagicDungeon/Assets/Scripts/Dungeon/InstantiatedRoom.cs
@@ -33,6 +33,7 @@ public class InstantiatedRoom : MonoBehaviour
     public void Initialise(GameObject roomGameObject)
     {
         PopulateTilemapMemberVariables(roomGameObject);
+        BlockOffUnusedDoorWays();
         DisableCollisionTilemapRenderer();
     }
 
@@ -79,12 +80,117 @@ public class InstantiatedRoom : MonoBehaviour
     }
 
     /// <summary>
+    /// Блокировка всех не испольщуемых проёмов на всех Tilemap
+    /// </summary>
+    private void BlockOffUnusedDoorWays()
+    {
+        foreach (Doorway doorway in room.doorWayList)
+        {
+            if (doorway.isConnected)
+                continue;
+
+            if (collisionTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(collisionTilemap, doorway);
+            }
+            if (minimapTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(minimapTilemap, doorway);
+            }
+            if (groundTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(groundTilemap, doorway);
+            }
+            if (decoration1Tilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(decoration1Tilemap, doorway);
+            }
+            if (decoration2Tilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(decoration2Tilemap, doorway);
+            }
+            if (frontTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(frontTilemap, doorway);
+            }
+            if (ignoreAmmoTilemap != null)
+            {
+                BlockDoorwayOnTilemapLayer(ignoreAmmoTilemap, doorway);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Блокирует дверной проём на Tilemap
+    /// </summary>
+    private void BlockDoorwayOnTilemapLayer (Tilemap tilemap, Doorway doorway)
+    {
+        switch (doorway.orientation)
+        {
+            case Orientation.north:
+            case Orientation.south:
+                BlockDoorwayVertically(tilemap, doorway);
+                break;
+            case Orientation.east:
+            case Orientation.west:
+                BlockDoorwayHorizontally(tilemap, doorway);
+                break;
+
+            case Orientation.none:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Блокирование вертикальных проёмов
+    /// </summary>
+    private void BlockDoorwayVertically(Tilemap tilemap, Doorway doorway)
+    {
+        Vector2Int startPos = doorway.doorwayStartCopyPos;
+
+        for (int xPos = 0; xPos < doorway.doorwayCopyTileWidth; xPos++)
+        {
+            for (int yPos = 0; yPos < doorway.doorwayCopyTileHeight; yPos++)
+            {
+                Matrix4x4 transformMatrix = tilemap.GetTransformMatrix(new Vector3Int(startPos.x + xPos, startPos.y - yPos, 0));
+
+                tilemap.SetTile(new Vector3Int(startPos.x + 1 + xPos, startPos.y - yPos, 0),
+                    tilemap.GetTile(new Vector3Int(startPos.x + xPos, startPos.y - yPos, 0)));
+                tilemap.SetTransformMatrix(new Vector3Int(startPos.x + 1 + xPos, startPos.y - yPos, 0), transformMatrix);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Блокирование горизонтальных проёмов
+    /// </summary>
+    private void BlockDoorwayHorizontally(Tilemap tilemap, Doorway doorway)
+    {
+        Vector2Int startPos = doorway.doorwayStartCopyPos;
+        for (int yPos = 0; yPos < doorway.doorwayCopyTileHeight; yPos++)
+        {
+            for (int xPos = 0; xPos < doorway.doorwayCopyTileWidth; xPos++)
+            {
+                Matrix4x4 transformMatrix = tilemap.GetTransformMatrix(new Vector3Int(startPos.x + xPos, startPos.y - yPos, 0));
+
+                tilemap.SetTile(new Vector3Int(startPos.x + xPos, startPos.y - 1 - yPos, 0),
+                    tilemap.GetTile(new Vector3Int(startPos.x + xPos, startPos.y - yPos, 0)));
+                tilemap.SetTransformMatrix(new Vector3Int(startPos.x + xPos, startPos.y - 1 - yPos, 0), transformMatrix);
+            }
+        }
+
+    }
+
+    /// <summary>
     /// Отключение визуализации уу слоя с коллизией
     /// </summary>
     private void DisableCollisionTilemapRenderer()
     {
         collisionTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
         ignoreAmmoTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
+        minimapTilemap.gameObject.GetComponent<TilemapRenderer>().enabled = false;
     }
 }
  
